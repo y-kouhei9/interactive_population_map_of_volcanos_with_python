127cdf7e9b43ac84a7f3748d390adcfa791d7513
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-10-01 10:44:07 +0900

    Exploring the countries by its population

--- map1.py
+++ map1.py
@@ -32,7 +32,8 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-map.add_child(fg)
+fg.add_child(folium.GeoJson(data=open('Webmap_datasources/world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
+map.add_child(fg)
 map.save("Map_html_popup_advanced.html")
 
