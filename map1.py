b5836f5756d4a39c870cac7272fbc30917575c79
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-10-01 10:19:54 +0900

    Stylized Markers to Circle

--- map1.py
+++ map1.py
@@ -14,6 +14,15 @@ Volcano name:<br>
 Height: %s m
 """
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, titles="Stamen Terrain")
 
@@ -21,7 +30,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
 map.add_child(fg)
 
