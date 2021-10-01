360a1b917524620e2890efabcada37411b3545e4
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-10-01 09:51:01 +0900

    Edit popup html and changed html name

--- map1.py
+++ map1.py
@@ -5,15 +5,22 @@ import pandas
 data = pandas.read_csv("Webmap_datasources/Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
 
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, titles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
-map.save("Map1.html")
+map.save("Map_html_popup_simple.html")
+
